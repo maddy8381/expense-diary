0c92edec6c1858a04ce0d03268b59f0d32a4ca88
unknown <sgmadankar@gmail.com>
2021-04-19 14:57:14 +0530

    Restructuring Folders

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Card.css';
 
 function Card(props) {
-    const classes = 'card ' + props.className;
+    const classes = 'card ' + props.className; //props.className receive all the classes we pass to Card tag
 
     return (
         <div className={classes}>
