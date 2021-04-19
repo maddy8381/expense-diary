a93af480bb48a341e4174e74d8197d2854f7eb6c
unknown <sgmadankar@gmail.com>
2021-04-19 13:48:23 +0530

    Added More ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const expenseDate = new Date(2021, 3, 19);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 298.5;
+
     return (
         <div className="expense-item">
-            <div>Mar 12th 2021</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$555</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     )
